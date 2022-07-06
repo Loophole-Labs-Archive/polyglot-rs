00a0ffadb6da25d8613e1106491873e6d2d587ac
Alex Sørlie Glomsaas <alex@sorlie.io>
2022-07-06 20:44:50 +0200

    Implement tests for decoder

--- tests/decoder.rs
+++ tests/decoder.rs
@@ -1,9 +1,240 @@
 extern crate polyglot;
+
+use std::collections::HashMap;
+use std::io::Cursor;
 use polyglot::Encoder;
 use polyglot::Decoder;
+use polyglot::DecodingError;
 use polyglot::Kind;
 
 #[test]
 fn test_decode_nil() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    encoder = encoder.encode_none();
 
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_none();
+    assert_eq!(val, true);
+    assert_eq!(decoder.get_ref().len() - decoder.position() as usize, 0);
+    let next_val = decoder.decode_none();
+    assert_eq!(next_val, false);
+}
+
+#[test]
+fn test_decode_array() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let m = ["1", "2", "3"];
+
+    encoder = encoder.encode_array(m.len(), Kind::String);
+    for i in m {
+        encoder = encoder.encode_string(i);
+    }
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let size = decoder.decode_array(Kind::String).unwrap() as usize;
+    assert_eq!(size, m.len());
+
+    let mut mv: Vec<String> = Vec::with_capacity(size);
+    for i in 0..size {
+        let val = decoder.decode_string().unwrap();
+        mv.push(val.to_string());
+        assert_eq!(mv[i], m[i]);
+    }
+    assert_eq!(mv, m);
+
+    let error = decoder.decode_array(Kind::String).unwrap_err();
+    assert_eq!(error, DecodingError::InvalidArray);
+}
+
+#[test]
+fn test_decode_map() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let mut m: HashMap<String, u32> = HashMap::new();
+    m.insert(String::from("1"), 1);
+    m.insert(String::from("2"), 2);
+    m.insert(String::from("3"), 3);
+
+    encoder = encoder.encode_map(m.len(), Kind::String, Kind::U32);
+    for (k, v) in m.clone() {
+        encoder = encoder.encode_string(k.as_str()).encode_u32(v);
+    }
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let size = decoder.decode_map(Kind::String, Kind::U32).unwrap() as usize;
+    assert_eq!(size, m.len());
+
+    let mut mv = HashMap::new();
+    for _ in 0..size {
+        let k = decoder.decode_string().unwrap();
+        let v = decoder.decode_u32().unwrap();
+        mv.insert(k, v);
+    }
+    assert_eq!(mv, m);
+
+    let error = decoder.decode_map(Kind::String, Kind::U32).unwrap_err();
+    assert_eq!(error, DecodingError::InvalidMap);
+}
+
+#[test]
+fn test_decode_bytes() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = "Test String".as_bytes();
+    encoder = encoder.encode_bytes(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let bytes = decoder.decode_bytes().unwrap();
+    assert_eq!(bytes, v);
+}
+
+#[test]
+fn test_decode_string() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = "Test String";
+    encoder = encoder.encode_string(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_string().unwrap();
+    assert_eq!(val, v);
+
+    let error = decoder.decode_string().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidString);
+}
+
+#[test]
+fn test_decode_error() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = "Test String";
+    encoder = encoder.encode_error(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_error().unwrap();
+    assert_eq!(val, v);
+
+    let error = decoder.decode_error().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidError);
+}
+
+#[test]
+fn test_decode_bool() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    encoder = encoder.encode_bool(true);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_bool().unwrap();
+    assert_eq!(val, true);
+
+    let error = decoder.decode_bool().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidBool);
+}
+
+#[test]
+fn test_decode_u8() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = 32 as u8;
+    encoder = encoder.encode_u8(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_u8().unwrap();
+    assert_eq!(val, v);
+
+    let error = decoder.decode_u8().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidU8);
+}
+
+#[test]
+fn test_decode_u16() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = 1024 as u16;
+    encoder = encoder.encode_u16(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_u16().unwrap();
+    assert_eq!(val, v);
+
+    let error = decoder.decode_u16().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidU16);
+}
+
+#[test]
+fn test_decode_u32() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = 4294967290 as u32;
+    encoder = encoder.encode_u32(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_u32().unwrap();
+    assert_eq!(val, v);
+
+    let error = decoder.decode_u32().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidU32);
+}
+
+#[test]
+fn test_decode_u64() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = 18446744073709551610 as u64;
+    encoder = encoder.encode_u64(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_u64().unwrap();
+    assert_eq!(val, v);
+
+    let error = decoder.decode_u64().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidU64);
+}
+
+#[test]
+fn test_decode_i32() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = -2147483648 as i32;
+    encoder = encoder.encode_i32(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_i32().unwrap();
+    assert_eq!(val, v);
+
+    let error = decoder.decode_i32().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidI32);
+}
+
+#[test]
+fn test_decode_i64() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = -9223372036854775808 as i64;
+    encoder = encoder.encode_i64(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_i64().unwrap();
+    assert_eq!(val, v);
+
+    let error = decoder.decode_i64().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidI64);
+}
+
+#[test]
+fn test_decode_f32() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = -2147483.648 as f32;
+    encoder = encoder.encode_f32(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_f32().unwrap();
+    assert_eq!(val, v);
+
+    let error = decoder.decode_f32().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidF32);
+}
+
+#[test]
+fn test_decode_f64() {
+    let mut encoder = Cursor::new(Vec::with_capacity(512));
+    let v = -922337203.477580 as f64;
+    encoder = encoder.encode_f64(v);
+
+    let mut decoder = Cursor::new(encoder.get_mut());
+    let val = decoder.decode_f64().unwrap();
+    assert_eq!(val, v);
+
+    let error = decoder.decode_f64().unwrap_err();
+    assert_eq!(error, DecodingError::InvalidF64);
 }
