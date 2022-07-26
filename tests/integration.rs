0df3c99d2d05b7e7a6fcf464c8eafe6812a8b73c
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-07-26 20:23:53 +0200

    Add more integration tests from data file

--- tests/integration.rs
+++ tests/integration.rs
@@ -2,7 +2,6 @@ extern crate polyglot;
 
 use lazy_static::lazy_static;
 use polyglot::Decoder;
-use polyglot::Encoder;
 use polyglot::Kind;
 use serde::Deserialize;
 use serde_json::Value;
@@ -59,15 +58,89 @@ fn test_encode() {
     let a: &mut Vec<TestData> = &mut TEST_DATA.lock().unwrap();
 
     for td in a {
+        let mut decoder = Cursor::new(td.encoded_value.as_mut());
+
         match td.kind {
             Kind::None => {
-                let mut decoder = Cursor::new(td.encoded_value.as_mut());
                 let val = decoder.decode_none();
 
                 if td.decoded_value.is_null() {
                     assert_eq!(val, true)
                 } else {
                     assert_eq!(val, false)
+                }
+            }
+
+            Kind::Bool => {
+                let val = decoder.decode_bool().unwrap();
+
+                assert_eq!(val, td.decoded_value.as_bool().unwrap());
+            }
+
+            Kind::U8 => {
+                let val = decoder.decode_u8().unwrap();
+
+                assert_eq!(val as u64, td.decoded_value.as_u64().unwrap());
+            }
+
+            Kind::U16 => {
+                let val = decoder.decode_u16().unwrap();
+
+                assert_eq!(val as u64, td.decoded_value.as_u64().unwrap());
+            }
+
+            Kind::U32 => {
+                let val = decoder.decode_u32().unwrap();
+
+                assert_eq!(val as u64, td.decoded_value.as_u64().unwrap());
+            }
+
+            Kind::U64 => {
+                let val = decoder.decode_u64().unwrap();
+
+                assert_eq!(val as u64, td.decoded_value.as_u64().unwrap());
+            }
+
+            Kind::I32 => {
+                let val = decoder.decode_i32().unwrap();
+
+                assert_eq!(val as i64, td.decoded_value.as_i64().unwrap());
+            }
+
+            Kind::I64 => {
+                let val = decoder.decode_i64().unwrap();
+
+                assert_eq!(val as i64, td.decoded_value.as_i64().unwrap());
+            }
+
+            Kind::F32 => {
+                let val = decoder.decode_f32().unwrap();
+
+                assert!(
+                    (val as f32 - td.decoded_value.as_f64().unwrap() as f32) < std::f32::EPSILON
+                );
+            }
+
+            Kind::F64 => {
+                let val = decoder.decode_f64().unwrap();
+
+                assert!(
+                    (val as f64 - td.decoded_value.as_f64().unwrap() as f64) < std::f64::EPSILON
+                );
+            }
+
+            Kind::Array => {
+                let len = decoder.decode_array(Kind::String).unwrap();
+
+                let expected = td.decoded_value.as_array().unwrap();
+
+                assert_eq!(expected.len(), len);
+
+                for (i, _) in expected.into_iter().enumerate() {
+                    assert_eq!(
+                        expected[i].as_str().unwrap(),
+                        decoder.decode_string().unwrap()
+                    )
                 }
             }
 
